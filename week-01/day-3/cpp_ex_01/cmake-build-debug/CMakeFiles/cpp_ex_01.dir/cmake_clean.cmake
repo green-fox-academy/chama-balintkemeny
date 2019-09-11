afb180638e21245e6bb9ad77b03ce45a4e73c712
balintkemeny <balintkemeny@gmail.com>
2019-09-11 11:49:36 +0200

    solved 1st cpp exercise

---
+++ week-01/day-3/cpp_ex_01/cmake-build-debug/CMakeFiles/cpp_ex_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_ex_01.dir/main.cpp.obj"
+  "cpp_ex_01.pdb"
+  "cpp_ex_01.exe"
+  "cpp_ex_01.exe.manifest"
+  "libcpp_ex_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_ex_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-01/day-3/cpp_ex_01/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+
+    //Greet me!
+    std::cout << "Hello, Balint!" << std::endl;
+
+    //Let's put an empty line here:
+    std::cout << "\n";
+
+    //Humpty Dumpty
+    std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+    std::cout << "Humpty Dumpty had a great fall." << std::endl;
+    std::cout << "All the king's horses and all the king's men" << std::endl;
+    std::cout << "Couldn't put Humpty together again." << std::endl;
+
+    //Let's put an empty line here:
+    std::cout << "\n";
+
+    //Hello Others!
+    std::cout << "Hi Stan!" << std::endl;
+    std::cout << "Hi Kyle!" << std::endl;
+    std::cout << "Hi Kenny!" << std::endl;
+
+    return 0;
+}
