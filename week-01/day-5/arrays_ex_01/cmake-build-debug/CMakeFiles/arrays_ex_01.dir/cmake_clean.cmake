04dbbd53c0340455027945f863a4e30154faeba3
balintkemeny <balintkemeny@gmail.com>
2019-09-13 13:07:20 +0200

    Solve arrays ex. 1

---
+++ week-01/day-5/arrays_ex_01/cmake-build-debug/CMakeFiles/arrays_ex_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arrays_ex_01.dir/main.cpp.obj"
+  "arrays_ex_01.pdb"
+  "arrays_ex_01.exe"
+  "arrays_ex_01.exe.manifest"
+  "libarrays_ex_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arrays_ex_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
