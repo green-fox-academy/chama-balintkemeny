6bcba8aa074e54eca323006e37a8264e35c7739f
balintkemeny <balintkemeny@gmail.com>
2019-09-17 16:03:16 +0200

    Solve anagram

--- week-01/day-5/functions_ex_09/main.cpp
+++ week-01/day-5/functions_ex_09/main.cpp
@@ -1,40 +1,23 @@
 #include <iostream>
-using namespace std;
+#include <algorithm>
 
-//ANAGRAM EXERCISE - YET TO BE RESOLVED!!!
+bool anagram (std::string inputA, std::string inputB);
 
 int main() {
-    std::cout << "" << std::endl;
+    std::string a = "fox";
+    std::string b = "bird";
+
+    std::cout << anagram(a, b) << std::endl;
+
     return 0;
 }
 
-bool anagram (string inputA, string inputB) {
+bool anagram (std::string inputA, std::string inputB) {
     bool ifAnagram = false;
-    int lengthA = inputA.length();
-    int lengthB = inputB.length();
-
-    if (lengthA == lengthB) {
-        /*for (int i = 0; i < lengthA; ++i) {
-            int j;
-            for (j = 0; j < lengthA; ++j) {
-                if (inputA[i] == inputB[j]) {
-
-                }
-            }
-        } */
-        int i = 0;
-        while (i < lengthA) {
-            int j = 0;
-            while (j < lengthA) {
-                if (inputA[i] == inputB[j]) {
-
-                }
-                j++;
-            }
-            i++;
-        }
-    } else {
-        ifAnagram = false;
+    if (inputA.length() == inputB.length()) {
+        std::sort(inputA.begin(), inputA.end());
+        std::sort(inputB.begin(), inputB.end());
+        ifAnagram = inputA == inputB;
     }
-    return  ifAnagram;
+    return ifAnagram;
 }
