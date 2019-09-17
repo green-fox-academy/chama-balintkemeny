82be6c6e1eca82545013d5945c053e872d83bc75
balintkemeny <balintkemeny@gmail.com>
2019-09-17 11:11:47 +0200

    Solve pointers ex. 5

--- week-02/day-2/pointers_05/main.cpp
+++ week-02/day-2/pointers_05/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+int* minpointer (int arr [], int len);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int testArray[5] = {3, 2, 5, 6, 7};
+    int testArraySize = 5;
+
+    int *ptrToMinTest = minpointer(testArray, testArraySize);
+
+    std::cout << &testArray[1] << " vs " << ptrToMinTest << std::endl;
+
     return 0;
+}
+
+int* minpointer (int arr[], int len) {
+    int min = arr[0];
+    int j = 0;
+    for (int i = 0; i < len; ++i) {
+        if (arr [i] < min) {
+            min = arr[i];
+            j = i;
+        }
+    }
+    return &arr[j];
 }
