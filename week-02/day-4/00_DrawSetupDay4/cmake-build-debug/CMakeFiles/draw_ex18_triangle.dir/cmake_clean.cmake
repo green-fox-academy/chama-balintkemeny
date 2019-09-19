93ac98e8ee8925511b845bcbbfbff4af418c0a7d
balintkemeny <balintkemeny@gmail.com>
2019-09-19 11:57:46 +0200

    Solved triangle drawing exercise, copied SuperHexagon to day-4

---
+++ week-02/day-4/00_DrawSetupDay4/cmake-build-debug/CMakeFiles/draw_ex18_triangle.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/draw_ex18_triangle.dir/main.cpp.obj"
+  "CMakeFiles/draw_ex18_triangle.dir/draw_ex18_triangle.cpp.obj"
+  "CMakeFiles/draw_ex18_triangle.dir/D_/SDL2/SDL2_plugins/gfx/src/SDL2_framerate.c.obj"
+  "CMakeFiles/draw_ex18_triangle.dir/D_/SDL2/SDL2_plugins/gfx/src/SDL2_gfxPrimitives.c.obj"
+  "CMakeFiles/draw_ex18_triangle.dir/D_/SDL2/SDL2_plugins/gfx/src/SDL2_imageFilter.c.obj"
+  "CMakeFiles/draw_ex18_triangle.dir/D_/SDL2/SDL2_plugins/gfx/src/SDL2_rotozoom.c.obj"
+  "draw_ex18_triangle.pdb"
+  "draw_ex18_triangle.exe"
+  "draw_ex18_triangle.exe.manifest"
+  "libdraw_ex18_triangle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/draw_ex18_triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day-4/00_DrawSetupDay4/draw_ex18_triangle.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Kemény Bálint on 2019. 09. 19..
+//
+#include "draw.h"
+#include <cmath>
+
+void drawTriangle (SDL_Renderer* gRenderer, int ix, int iy, int iside) {
+    auto x = (double)ix;
+    auto y = (double)iy;
+    auto side = (double)iside;
+
+    double A[2] = {x, y};
+    double B[2] = {x + side, y};
+    double C[2] = {x + side / 2, y - side * pow(3.0, 0.5) / 2};
+
+    SDL_RenderDrawLine(gRenderer, (int)A[0], (int)A[1], (int)B[0], (int)B[1]);
+    SDL_RenderDrawLine(gRenderer, (int)B[0], (int)B[1], (int)C[0], (int)C[1]);
+    SDL_RenderDrawLine(gRenderer, (int)C[0], (int)C[1], (int)A[0], (int)A[1]);
+}
+
+void draw(SDL_Renderer* gRenderer) {
+    int startX = 10;
+    int startY = SCREEN_HEIGHT - 10;
+    int triangleCount = 21;
+    int triangleSize = (SCREEN_HEIGHT - 20) / triangleCount;
+
+    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+    drawTriangle(gRenderer, startX, startY, triangleSize);
+
+    while (triangleCount > 0) {
+        for (int i = 0; i < triangleCount; ++i) {
+            drawTriangle(gRenderer, startX, startY, triangleSize);
+            int incrX = triangleSize;
+            startX += incrX;
+        }
+        startX = (int)(startX - (triangleCount - 0.5) * triangleSize);
+        startY -= (int)(triangleSize * pow(3.0, 0.5) / 2);
+        triangleCount--;
+    }
+
+
+}
