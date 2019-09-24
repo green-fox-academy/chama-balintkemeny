3ea52b31eef0183b614f331dbfeb34b567b3722f
balintkemeny <balintkemeny@gmail.com>
2019-09-24 15:28:28 +0200

    solved ex_07 - copyfiles on week-03/day-2

---
+++ week-03/day-2/ex07_copyFiles/cmake-build-debug/CMakeFiles/ex07_copyFiles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex07_copyFiles.dir/main.cpp.obj"
+  "ex07_copyFiles.pdb"
+  "ex07_copyFiles.exe"
+  "ex07_copyFiles.exe.manifest"
+  "libex07_copyFiles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex07_copyFiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-03/day-2/ex07_copyFiles/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+bool copyFile (std::string pathIn, std::string pathOut) {
+    std::ifstream inputFile;
+    std::ofstream outputFile;
+    std::string line;
+
+    inputFile.open(pathIn);
+    outputFile.open(pathOut);
+
+    if (inputFile.is_open() && outputFile.is_open()) {
+        while(!inputFile.eof()) {
+            std::getline(inputFile, line);
+            outputFile << line << std::endl;
+        }
+        return  true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    std::string pathIn = "input.txt";
+    std::string pathOut = "output.txt";
+    bool ifSuccess = copyFile(pathIn, pathOut);
+    std::cout << ifSuccess << std::endl;
+
+    return 0;
+}
