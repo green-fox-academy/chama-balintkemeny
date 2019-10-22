0b4702d7e1947d2417fce64b11d0c76e4e93af66
balintkemeny <balintkemeny@gmail.com>
2019-10-22 15:42:48 +0200

    Fixed array rotator mistake

--- week-06/day-2/ex08_arrayRotator/main.c
+++ week-06/day-2/ex08_arrayRotator/main.c
@@ -40,6 +40,7 @@ void array_rotator(void *ptr, uint32_t bytes, uint32_t rotation_count, uint8_t r
                 if (i != bytes - 1) {
                     array[i] |= tmp;
                 } else {
+                    array[i] |= tmp;
                     array[0] |= tmp2;
                 }
                 tmp = tmp2;
@@ -59,6 +60,7 @@ void array_rotator(void *ptr, uint32_t bytes, uint32_t rotation_count, uint8_t r
                 if (i != 0) {
                     array[i] |= tmp;
                 } else {
+                    array[0] |= tmp;
                     array[bytes - 1] |= tmp2;
                     break;
                 }
@@ -72,19 +74,16 @@ int main()
 {
     //test case bytes = 3
     uint8_t test_byte1 = 0b01001010;
-    uint8_t test_byte2 = 0b11110000;
+    uint8_t test_byte2 = 0b11110011;
     uint8_t test_byte3 = 0b10010110;
     uint8_t test_bytes[3] = {test_byte1, test_byte2, test_byte3};
-    
-    for (uint8_t i = 0; i < 3; ++i) {
-        print_binary(test_bytes[i]);
-    }
-    printf("\n");
 
-    array_rotator(test_bytes, 3, 2, 1);
-
-    for (uint8_t i = 0; i < 3; ++i) {
-        print_binary(test_bytes[i]);
+    for (uint8_t j = 0; j < 16; ++j) {
+        array_rotator(test_bytes, 3, j, 1);
+        for (uint8_t i = 0; i < 3; ++i) {
+            print_binary(test_bytes[i]);
+        }
+        printf("\n");
     }
 
     return 0;
