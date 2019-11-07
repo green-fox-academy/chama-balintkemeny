fb77953d27856b91eba5c76a11194855a1792c58
balintkemeny <balintkemeny@gmail.com>
2019-11-07 16:03:18 +0100

    Solve ADC ex01

---
+++ week-08/day-4/ex01_joystick/ex01_joystick/main.c
@@ -0,0 +1,40 @@
+/*
+ * ex01_joystick.c
+ *
+ * Created: 2019. 11. 07. 15:27:26
+ * Author : b_kemeny
+ */ 
+
+#include <avr/io.h>
+
+
+void init()
+{
+	//Set PWM out on PD6
+	DDRD = 0b01000000;
+	//Set timer control register A - fast PWM
+	TCCR0A = 0b10000011;
+	//Set timer 0 pre-scaler
+	TCCR0B = 0b00000010;
+	
+	//Set ADC Enable
+	ADCSRA = 0b11100000;
+	//Set left adjust
+	ADMUX = 0b00100000;
+}
+
+void set_duty()
+{
+	OCR0A = ADCH;
+}
+
+int main(void)
+{
+	init();
+	
+    while (1) 
+    {
+		set_duty();
+	}
+}
+
